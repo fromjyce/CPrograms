fb09411f5a78cf8bf06810c454305b2490ddb1b6
Jayashre ‎ <128211962+fromjyce@users.noreply.github.com>
2023-06-17 17:09:09 +0530

    Add files via upload

---
+++ Assignments/MaxOfThree.c
@@ -0,0 +1,20 @@
+//Maximum of three numbers
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b, &c);
+    if(a>b && a<c)
+        {
+            printf("%d is greater number",a);
+        }
+    else if (b>a && b>c)
+        {
+            printf("%d is greater number",b);
+        }
+    else
+        {
+            printf("%d is greater number",c);
+        }    
+    return 0;
+}
